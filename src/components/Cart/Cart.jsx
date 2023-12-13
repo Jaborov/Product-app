d808bee5aad3fd63527fd1d6e29c9d5a1d7c5443
Jaborov <dzhaborov.b@gmail.com>
2023-12-13 11:22:28 +0500

    created cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { cartActions } from '../../store/cart.slice';
+
+function Cart() {
+	return (
+		<p>Hello</p>
+	);
+}
+
+export default Cart;
